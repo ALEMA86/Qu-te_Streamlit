bcc6383cf87a26391fef85089523f754caa149cc
ALEMA86 <aurore.lemaitre@gmail.com>
2022-01-10 13:58:14 +0100

    Update streamlit_app_test.py

--- Streamlit/streamlit_app_test.py
+++ Streamlit/streamlit_app_test.py
@@ -7,11 +7,11 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import plotly.express as px
 import ipywidgets as widgets
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
-import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 
 
